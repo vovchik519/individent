72946fb463fb80438d10a88cde3e8ba3496132a8
Vovchik <vova2000ra@mail.ru>
2023-02-28 21:02:06 +0600

    metadata and header

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+function toggleAddress() {
+    var address = document.getElementById("address");
+    if (address.style.display === "none") {
+        address.style.display = "block";
+    } else {
+        address.style.display = "none";
+    }
+}
