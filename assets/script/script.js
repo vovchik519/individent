08c71407e2c67955ad4e91296ba5f340d6a87c35
Vovchik <vova2000ra@mail.ru>
2023-02-28 23:12:47 +0600

    header

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,3 +6,30 @@ function toggleAddress() {
         address.style.display = "none";
     }
 }
+
+var swiper = new Swiper(".firstscreen__swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 'auto',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+});
+
+var header = document.getElementsByTagName("header")[0];
+var headerHeight = header.offsetHeight;
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos || currentScrollPos <= headerHeight) {
+        header.classList.remove("hide");
+    } else {
+        header.classList.add("hide");
+    }
+    prevScrollpos = currentScrollPos;
+}
