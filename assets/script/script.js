7962b28536302894867ba6d81330bb96368dcfa8
Vovchik <vova2000ra@mail.ru>
2023-03-03 20:04:27 +0600

    animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -132,3 +132,11 @@ elements.forEach(element => {
     });
     observer.observe(element);
 });
+
+window.addEventListener('scroll', function () {
+    const parallaxItems = document.querySelectorAll('.icons__item');
+    let scrollPosition = window.pageYOffset;
+    parallaxItems.forEach(function (parallax) {
+        parallax.style.transform = 'translateY(' + scrollPosition * 0.1 + 'px)';
+    });
+});
