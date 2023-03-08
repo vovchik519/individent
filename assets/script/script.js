cfc830e8c74c02ad0812028e90d21325ad46e158
Vovchik <vova2000ra@mail.ru>
2023-03-08 10:33:32 +0600

    fix

--- assets/script/script.js
+++ assets/script/script.js
@@ -356,3 +356,19 @@ function toggleMark1() {
         tooltip1.style.display = "none";
     }
 };
+
+var toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 50) {
+        toTop.classList.add('active');
+    } else {
+        toTop.classList.remove('active');
+    }
+});
+
+toTop.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
